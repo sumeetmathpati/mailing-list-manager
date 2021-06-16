7962e771be20f90988a62be13e9d1049b628ce07
Sumeet Mathpati <sumeet.mathapti@gmail.com>
2021-06-16 11:06:27 -0400

    added connection.php

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+    define('DB_SERVER','localhost');
+    define('DB_USER','sumeet');
+    define('DB_PASS' ,'PASS');
+    define('DB_NAME', 'mailserver');
+    $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+    
+    // Check connection
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+?>
